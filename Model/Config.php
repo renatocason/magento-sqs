ae3a7f7d31038d2c9f2b57ec73655f1530d203c0
Giorgio Trobbiani <giorgio@cooder.it>
2020-03-20 17:10:40 +0100

    Add system configs

--- Model/Config.php
+++ Model/Config.php
@@ -10,6 +10,8 @@ namespace Belvg\Sqs\Model;
 use Enqueue\Sqs\SqsConnectionFactory;
 use Enqueue\Sqs\SqsContext;
 use Magento\Framework\App\DeploymentConfig;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 /**
  * Reads the SQS config in the deployed environment configuration
@@ -33,6 +35,11 @@ class Config
     const PREFIX = 'prefix';
     const ENDPOINT = 'endpoint';
 
+    const XML_PATH_SQS_QUEUE_REGION = 'system/sqs/region';
+    const XML_PATH_SQS_QUEUE_VERSION = 'system/sqs/version';
+    const XML_PATH_SQS_QUEUE_ACCESS_KEY = 'system/sqs/access_key';
+    const XML_PATH_SQS_QUEUE_SECRET_KEY = 'system/sqs/secret_key';
+
     /**
      * Deployment configuration
      *
@@ -44,6 +51,11 @@ class Config
      * @var SqsClient
      */
     private $connection;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
 
     /**
      * @var array
@@ -77,9 +89,12 @@ class Config
      *
      * @param DeploymentConfig $config
      */
-    public function __construct(DeploymentConfig $config)
-    {
+    public function __construct(
+        DeploymentConfig $config,
+        ScopeConfigInterface $scopeConfig
+    ){
         $this->deploymentConfig = $config;
+        $this->scopeConfig = $scopeConfig;
     }
 
     /**
@@ -110,12 +125,18 @@ class Config
      */
     public function getValue($key)
     {
-        $this->load();
+        // Load the configuration for SQS from system configs
+        $this->loadSystemConfigs();
+        
+        // If no configuration is found, load the configuration from deployment configs
+        if (null === $this->data) {
+            $this->load();
+        }
         return isset($this->data[$key]) ? $this->data[$key] : null;
     }
 
     /**
-     * Load the configuration for SQS
+     * Load the configuration for SQS from deployment configs
      *
      * @return void
      */
@@ -124,6 +145,21 @@ class Config
         if (null === $this->data) {
             $queueConfig = $this->deploymentConfig->getConfigData(self::QUEUE_CONFIG);
             $this->data = isset($queueConfig[self::SQS_CONFIG]) ? $queueConfig[self::SQS_CONFIG] : [];
+        }
+    }
+
+    /**
+     * Load the configuration for SQS from system configs
+     *
+     * @return void
+     */
+    private function loadSystemConfigs()
+    {
+        if (null === $this->data) {
+            $this->data[self::REGION] = $this->scopeConfig->getValue(self::XML_PATH_SQS_QUEUE_REGION,ScopeInterface::SCOPE_STORE);
+            $this->data[self::VERSION] = $this->scopeConfig->getValue(self::XML_PATH_SQS_QUEUE_VERSION,ScopeInterface::SCOPE_STORE);
+            $this->data[self::ACCESS_KEY] = $this->scopeConfig->getValue(self::XML_PATH_SQS_QUEUE_ACCESS_KEY,ScopeInterface::SCOPE_STORE);
+            $this->data[self::SECRET_KEY] = $this->scopeConfig->getValue(self::XML_PATH_SQS_QUEUE_SECRET_KEY,ScopeInterface::SCOPE_STORE);
         }
     }
 }
